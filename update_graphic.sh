1820b2636c7e6c17c1b9fadddd6db5cb723f0308
wen <wen@trackview.co>
2019-04-15 18:43:26 +0800

    create temp svg to avoid refresh error
    TEST: ./update_graphic.sh

--- update_graphic.sh
+++ update_graphic.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 $(./sqlite3.sh) $(./get-db-name.sh) \
     -header -csv "select * from web_ping order by id desc limit 100;" > usage.csv
-$(./gnuplot.sh) usage.plot > out.svg
+$(./gnuplot.sh) usage.plot > web/outing.svg
+
+cd web
+mv outing.svg out.svg
