45ee335ada3205c7e2c1cb3d9f2bb75029a76392
wen <wen@trackview.co>
2019-04-16 09:58:34 +0800

    show data in two days
    TEST: ./update_graphic.sh

--- update_graphic.sh
+++ update_graphic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 $(./sqlite3.sh) $(./get-db-name.sh) \
-    -header -csv "select id, web_site, ping, test_time, datetime(test_time, 'unixepoch', 'localtime') from web_ping where date(test_time, 'unixepoch', 'localtime') > date('now', '-1 day', 'localtime') order by id;" > usage.csv
+    -header -csv "select id, web_site, ping, test_time, datetime(test_time, 'unixepoch', 'localtime') from web_ping where date(test_time, 'unixepoch', 'localtime') > date('now', '-2 day', 'localtime') order by id;" > usage.csv
 $(./gnuplot.sh) usage.plot > web/outing.svg
 
 cd web
