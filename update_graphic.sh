b2df36eeb085adff484a7c9f01064f106bfcb11a
wen <wen@trackview.co>
2019-04-17 23:14:42 +0800

    generate one hour graphic
    TEST: ./update_graphic.sh

--- update_graphic.sh
+++ update_graphic.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 $(./sqlite3.sh) $(./get-db-name.sh) \
+    -header -csv "select id, web_site, ping, test_time from web_ping where test_time > (strftime('%s','now') - 3600) order by id;" > one_hour.csv
+$(./gnuplot.sh) usage.plot > web/one_hour_temp.svg
+
+$(./sqlite3.sh) $(./get-db-name.sh) \
     -header -csv "select id, web_site, ping, test_time from web_ping where test_time > (strftime('%s','now') - 172800) order by id;" > usage.csv
 $(./gnuplot.sh) usage.plot > web/outing.svg
 
 cd web
 mv outing.svg out.svg
+mv one_hour_temp.svg one_hour.svg
