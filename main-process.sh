3de5af6f2c3dfb6c358f05a8ae960419ec8222ff
wen <wen@trackview.co>
2019-04-17 08:19:03 +0800

    test https version site
    TEST: ./start-monitor.sh

--- main-process.sh
+++ main-process.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 ./create-db-if-needed.sh
 while true; do
-    ./test-single-site.sh "www.github.com"
-    ./test-single-site.sh "www.apple.com"
-    ./test-single-site.sh "www.google.com"
+    ./test-single-site.sh "https://www.github.com"
+    ./test-single-site.sh "https://www.apple.com"
+    ./test-single-site.sh "https://www.google.com"
+    ./test-single-site.sh "https://www.youtube.com/"
     ./update_graphic.sh
     sleep 60;
 done
