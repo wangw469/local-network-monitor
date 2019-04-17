3ca0e233dc519cb8ab7a6c4753c461aeb1418152
wen <wen@trackview.co>
2019-04-17 23:19:28 +0800

    fix one hour graphic generate error
    TEST: ./update_graphic.sh

--- update_graphic.sh
+++ update_graphic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 $(./sqlite3.sh) $(./get-db-name.sh) \
     -header -csv "select id, web_site, ping, test_time from web_ping where test_time > (strftime('%s','now') - 3600) order by id;" > one_hour.csv
-$(./gnuplot.sh) usage.plot > web/one_hour_temp.svg
+$(./gnuplot.sh) one_hour.plot > web/one_hour_temp.svg
 
 $(./sqlite3.sh) $(./get-db-name.sh) \
     -header -csv "select id, web_site, ping, test_time from web_ping where test_time > (strftime('%s','now') - 172800) order by id;" > usage.csv
